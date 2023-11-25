54ef5aa0d06abb0acd4cdd0870111e4b3679f2cd
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-11-25 18:44:14 -0300

    chore: create Dockerfiles and docker-compose.yml

--- go-producer/internal/producer/producer.go
+++ go-producer/internal/producer/producer.go
@@ -1,1 +1,23 @@
 package producer
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+func connectToBroker() (*amqp.Connection, error) {
+	return amqp.Dial("amqp://guest:guest@localhost:5672/")
+}
+
+func getChannel(conn *amqp.Connection) (*amqp.Channel, error) {
+	return conn.Channel()
+}
+
+func declareTopicExchange(channel *amqp.Channel, exchangeName string) error {
+	return channel.ExchangeDeclare(
+		exchangeName,
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+}
