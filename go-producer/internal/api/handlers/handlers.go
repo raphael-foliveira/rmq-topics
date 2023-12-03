54109dc085d8085bb786fd80ef6319c37c9e4b9e
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-12-03 17:35:53 -0300

    feat: receive messages in post endpoints

--- go-producer/internal/api/handlers/handlers.go
+++ go-producer/internal/api/handlers/handlers.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
 	"go-producer/internal/api/res"
 	"go-producer/internal/producer"
+	"io"
 	"net/http"
 )
 
@@ -18,21 +20,58 @@ func NewQueueHandler(queueProducer *producer.Publisher) *queueHandler {
 	return &queueHandler{queueProducer}
 }
 
+type topicOneMessage struct {
+	Message string `json:"message"`
+	ID      string `json:"id"`
+}
+
 func (qh *queueHandler) TopicOne(w http.ResponseWriter, r *http.Request) error {
-	qh.publisher.Publish(&producer.Message{
+	contentBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return &res.HttpError{
+			Status:  http.StatusBadRequest,
+			Message: err.Error(),
+		}
+	}
+	err = qh.publisher.Publish(&producer.Message{
 		TopicName: "topic.one",
-		Content:   []byte("this is a message to topic one"),
+		Content:   contentBytes,
 	})
+	if err != nil {
+		return &res.HttpError{
+			Status:  http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+	}
 	return res.Json(w, http.StatusOK, standardMessage{
 		Message: "sent!",
 	})
 }
 
+type topicTwoMessage struct {
+	Log              string `json:"log"`
+	CriticalityLevel string `json:"criticality_level"`
+}
+
 func (qh *queueHandler) TopicTwo(w http.ResponseWriter, r *http.Request) error {
-	qh.publisher.Publish(&producer.Message{
+	content := topicTwoMessage{}
+	err := json.NewDecoder(r.Body).Decode(&content)
+	if err != nil {
+		return &res.HttpError{
+			Status:  http.StatusBadRequest,
+			Message: err.Error(),
+		}
+	}
+	err = qh.publisher.Publish(&producer.Message{
 		TopicName: "topic.two",
 		Content:   []byte("this is a message to topic one"),
 	})
+	if err != nil {
+		return &res.HttpError{
+			Status:  http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+	}
 	return res.Json(w, http.StatusOK, standardMessage{
 		Message: "sent!",
 	})
--- go-producer/internal/api/server/server.go
+++ go-producer/internal/api/server/server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"go-producer/internal/api/handlers"
+	"go-producer/internal/api/res"
 	"go-producer/internal/producer"
 	"log"
 	"net/http"
@@ -21,15 +22,6 @@ func NewServer(port int) *server {
 	return &server{port}
 }
 
-type HttpError struct {
-	Status  int
-	Message string
-}
-
-func (e *HttpError) Error() string {
-	return e.Message
-}
-
 func (s *server) Start(queueProducer *producer.Publisher) error {
 	port := fmt.Sprintf(":%d", s.port)
 	router := chi.NewRouter()
@@ -44,8 +36,8 @@ func (s *server) Start(queueProducer *producer.Publisher) error {
 	router.Get("/", wrapHandler(handlers.HealthCheck))
 
 	queueHandler := handlers.NewQueueHandler(queueProducer)
-	router.Get("/topic-one", wrapHandler(queueHandler.TopicOne))
-	router.Get("/topic-two", wrapHandler(queueHandler.TopicTwo))
+	router.Post("/topic-one", wrapHandler(queueHandler.TopicOne))
+	router.Post("/topic-two", wrapHandler(queueHandler.TopicTwo))
 
 	log.Println("Server listening on port", port)
 	return http.ListenAndServe(port, router)
@@ -55,7 +47,7 @@ func wrapHandler(handler func(w http.ResponseWriter, r *http.Request) error) htt
 	return func(w http.ResponseWriter, r *http.Request) {
 		err := handler(w, r)
 		if err != nil {
-			httpError, ok := err.(*HttpError)
+			httpError, ok := err.(*res.HttpError)
 			if ok {
 				w.WriteHeader(httpError.Status)
 				json.NewEncoder(w).Encode(map[string]string{"error": httpError.Message})
